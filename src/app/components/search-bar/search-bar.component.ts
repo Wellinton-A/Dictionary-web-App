aed744776a6d0eac0a44ee15056942fc2cff8193
Wellinton Alexandre <wellinton1067@gmail.com>
2024-02-03 10:44:07 -0300

    feat: added style to field validation

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,10 +1,10 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { NgOptimizedImage } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { DarkModeService } from '../../services/dark-mode.service';
 import { ApiService } from '../../services/api.service';
 import { ActualWordService } from '../../services/actual-word.service';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'search-bar',
@@ -41,16 +41,27 @@ export class SearchBarComponent {
   #apiService = inject(ApiService)
   #wordService = inject(ActualWordService)
 
+  public word = new FormControl<string>(
+    '',
+    Validators.required
+  )
   public darkMode = this.#darkModeService.getDarkMode
-  public word = new FormControl<string>('')
+  public validField = signal<boolean>(true)
 
   public searchWord() {
-    if(this.#wordService.getWord()?.word !== this.word.value) {
+    if(!this.word.valid) {
+      this.validField.set(!this.validField())
+    }
+    if(this.#wordService.getWord()?.word !== this.word.value && this.word.valid) {
       this.#apiService.getWord(this.word.value!).subscribe({
         next: next => this.#wordService.handleWord(next[0], null),
         error: error => this.#wordService.handleWord(null, error.error),
         complete: () => this.#wordService.handleAudio()
       })
     }
+  }
+
+  public onInputBlur() {
+    this.validField.set(true)
   }
 }
